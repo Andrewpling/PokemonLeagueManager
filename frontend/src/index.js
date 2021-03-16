504f74f9fa70391751c3e9c2fa148c60fba7ba6b
Andrewpling <61208790+Andrewpling@users.noreply.github.com>
2021-03-16 11:35:23 -0400

    Added json file functionality

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,75 +2,30 @@ const Discord = require("discord.js");
 const config = require("./config.json");
 const fs = require('fs');
 
-
-
-
-/*const fs = require('fs') //importing file save
-var userPath = 'file path to json here'
-var userRead = fs.readFileSync(userPath
-);
-var userFile = JSON.parse(userRead); //ready for use
-var userId = message.author.id //user id here
-if (!userFile[userId]) { //this checks if data for the user has already been created
-    userFile[userId] = {xpp: 0, xppr: 0, currentRole: ""}; //if not, create it
-    fs.writeFileSync(userPath
-    , JSON.stringify(userFile, null, 2));
-} else {
-    //as an example, I will give the owner of the id 50 xp and the role "Awesome Role"
-    var xppVar = Number(userFile.xpp) + 50 //add 50 to their original xp
-    var xpprVar = Number(userFile.xppr)
-    var roleToGive = "Awesome Role"
-    userFile[userId] = {xpp: xppVar, xppr: xpprVar, currentRole: roleToGive};
-    fs.writeFileSync(userPath
-    , JSON.stringify(userFile, null, 2));
-    console.log(`Changed that player's xp to ${xppVar} and gave him the role ${roleToGive}`)
-
-}*/
-
 const client = new Discord.Client();
 
 const prefix = "=";
 
-const commandList = ["help", "info", "lose", "name", "ping", "win"];
+const commandList = ["help", "info", "lose", "name", "ping", "register", "reset", "win"];
 
-player = {
-  playerName: "",
-  playerWins: 0,
-  playerLosses: 0,
-  playerAvg: 0.0,
-};
-
-/* else {
-  //as an example, I will give the owner of the id 50 xp and the role "Awesome Role"
-  var xppVar = Number(userFile.xpp) + 50 //add 50 to their original xp
-  var xpprVar = Number(userFile.xppr)
-  var roleToGive = "Awesome Role"
-  userFile[userId] = {xpp: xppVar, xppr: xpprVar, currentRole: roleToGive};
-  fs.writeFileSync(userPath
-  , JSON.stringify(userFile, null, 2));
-  console.log(`Changed that player's xp to ${xppVar} and gave him the role ${roleToGive}`)
-
-}*/
-
-function reset(){
-  player.playerWins = 0;
-  player.playerLosses = 0;
-  player.playerAvg = 0;
-};
-
-function average(){
-  if(player.playerWins != 0 && player.playerLosses != 0){
-    player.playerAvg = player.playerWins / player.playerLosses;
+//function to average players' wins and losses
+function average(w, l){
+  if(w != 0 && l != 0){
+    return w/l;
+  } 
+  else{
+    return w;
   }
 };
 
+//function to display a full list of commands
 function displayCommands(){
   for(var i = 0; i < commandList.length; i++){
     return `${prefix}${commandList[i]}`;
   }
 };
 
-
+//begins bot functionality
 client.on("message", function(message) {
     if (message.author.bot) return;
     if (!message.content.startsWith(prefix)) return;
@@ -79,34 +34,71 @@ client.on("message", function(message) {
     const args = commandBody.split(' ');
     const command = args.shift().toLowerCase();
 
+    var userPath = '../../backend/UserData.json';
+    var userRead = fs.readFileSync(userPath);
+    var userFile = JSON.parse(userRead); 
+    var userId = message.author.id;
+
+    //test command to ping the server and display response time
     if (command === "ping") {
-        const timeTaken = Date.now() - message.createdTimestamp;
-        message.reply(`Pong! This message had a latency of ${timeTaken}ms.`);
-      }
+      const timeTaken = Date.now() - message.createdTimestamp;
+      message.reply(`Pong! This message had a latency of ${timeTaken}ms.`);
+    }
 
+    //command to change your username
     else if (command === "name") {
-      const name = args.toString();
-      player.playerName = name;
-      reset();
-      message.reply(`Player name updated to ${player.playerName}!`);
+      if (!userFile[userId]) {
+        message.reply("User not found. Please register first.") //this checks if data for the user has already been created
+      } else {
+      var newName = args.toString();
+      userFile[userId].name = newName;
+      fs.writeFileSync(userPath, JSON.stringify(userFile, null, 2));
+      message.reply(`Player name updated to ${userFile[userId].name}!`);
+      }
     }
 
+    //command to add a win
     else if (command === "win"){
-      const name = args.toString();
+      //var userId = message.author.id;
+      if (!userFile[userId]) {
+        message.reply("User not found. Please register first.") //this checks if data for the user has already been created
+      } else {
+        //as an example, I will give the owner of the id 50 xp and the role "Awesome Role"
+        //var nameVar = String(userFile[userId].name); //add 50 to their original xp
+        //var winVar = Number(userFile[userId].win) + 1;
+        //var lossVar = Number(userFile[userId].loss);
+        //userFile[userId] = {name: nameVar, win: winVar, loss: lossVar};
+        userFile[userId].win = userFile[userId].win + 1;
+        userFile[userId].avg = average(userFile[userId].win, userFile[userId].loss);
+        fs.writeFileSync(userPath, JSON.stringify(userFile, null, 2));
+        //console.log(`Changed that player's xp to ${xppVar} and gave him the role ${roleToGive}`)
+    
+    
+      /*const name = message.author.id;
       player.playerWins++;
-      average();
-      message.reply(`Congrats, ${player.playerName}, you now have ${player.playerWins} total wins!`);
+      average();*/
+        message.reply(`Congrats, ${userFile[userId].name}, you now have ${userFile[userId].win} total wins!`);
+        
+      }
     }
 
+    //command to add a loss
     else if (command === "lose"){
-      const name = args.toString();
-      player.playerLosses++;
-      average();
-      message.reply(`Too bad, ${player.playerName}, you now have ${player.playerLosses} total losses.`);
+      if (!userFile[userId]) {
+        message.reply("User not found. Please register first.") //this checks if data for the user has already been created
+      } else {
+        userFile[userId].loss = userFile[userId].loss + 1;
+        userFile[userId].avg = average(userFile[userId].win, userFile[userId].loss);
+        fs.writeFileSync(userPath, JSON.stringify(userFile, null, 2));
+        message.reply(`That's too bad, ${userFile[userId].name}, you now have ${userFile[userId].loss} losses.`)
+        //average(userFile, userId);
+      }
+      
     }
 
+    //display list of player info for a single user
     else if (command === "info"){
-      message.reply(`Player Name: ${player.playerName}\n` + `Wins: ${player.playerWins}\n` + `Losses: ${player.playerLosses}\n` + `W/L Ratio: ${player.playerAvg}`);
+      message.reply(`\nPlayer Name: ${userFile[userId].name}\n` + `Wins: ${userFile[userId].win}\n` + `Losses: ${userFile[userId].loss}\n` + `W/L Ratio: ${userFile[userId].avg}`);
     }
 
     else if (command === "help"){
@@ -119,19 +111,27 @@ client.on("message", function(message) {
       
     }
 
+    //command to register a user
     else if (command === "register"){
       var userPath = '../../backend/UserData.json';
       var userRead = fs.readFileSync(userPath);
       var userFile = JSON.parse(userRead);
       var userId = message.author.id;
       if (!userFile[userId]) { //this checks if data for the user has already been created
-        userFile[userId] = {name: "", win: 0, loss: 0}; //if not, create it
+        userFile[userId] = {name: "N/A", win: 0, loss: 0}; //if not, create it
         fs.writeFileSync(userPath, JSON.stringify(userFile, null, 2));
         message.reply("You have been registered!");
       }
       else{
         message.reply("User already registered")
       }
+    }
+
+    //command to clear player data
+    else if (command === "reset"){
+      userFile[userId] = {name: "N/A", win: 0, loss: 0, avg: 0};
+      fs.writeFileSync(userPath, JSON.stringify(userFile, null, 2));
+      message.reply("Name and stats reset.")
     }
 
 
